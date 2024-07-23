4df2123fa95e70d76de2587aa6e115b2e6fb44df
Marcelo Amador <marcelo@cklabs.dev>
2024-07-23 16:14:45 -0300

    Brand new project for testing MAUI permissions

---
+++ MAUIPermissions/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MAUIPermissions
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
